934d4b44f0125c92fc810e4c9bbc6c3d8e389833
CaioAdania <caioadania@gmail.com>
2025-07-30 21:00:18 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectTwo.Models;
+
+namespace ProjectTwo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Itens> Itens { get; set; }
+        public DbSet<Clients> Clients { get; set; }
+    }
+}
---
+++ Models/Itens.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ProjectTwo.Models
+{
+    public class Itens
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Amout { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool StateCode { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
