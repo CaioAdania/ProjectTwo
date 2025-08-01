ef98378bbae2299ef0d7c37f06eaeef597bf1800
CaioAdania <caioadania@gmail.com>
2025-08-01 17:36:46 -0300

    crud basico para itens

--- Controllers/ItensController.cs
+++ Controllers/ItensController.cs
@@ -1,11 +1,113 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+using ProjectTwo.Data;
+using ProjectTwo.Models;
 
 namespace ProjectTwo.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ItensController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public ItensController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetItens")]
+        public async Task<ActionResult<List<Itens>>> GetItens()
+        {
+            var itens = await _context.Itens.ToListAsync();
+
+            return Ok(itens);
+        }
+
+        [HttpPost]
+        [Route("AddItem")]
+        public async Task<ActionResult<Itens>> AddItens(Itens itens)
+        {
+            _context.Itens.Add(itens);
+            await _context.SaveChangesAsync();
+
+            return Ok(itens);
+        }
+
+        [HttpPut]
+        [Route("InactiveItem")]
+        public async Task<ActionResult<Itens>> InactiveItem(int id)
+        {
+            var inactive = _context.Itens.Where(i => i.Id == id).FirstOrDefault();
+
+            if(inactive != null)
+            {
+                if(inactive.Amout != 0)
+                {
+                    return Ok(new { warning = "Não é possível inativar produtos que possuem estoque." });
+                }
+                else
+                {
+                    inactive.StateCode = false;
+                    inactive.IsDeleted = true;
+                    inactive.DeletedOn = DateTime.Now;
+
+                    await _context.SaveChangesAsync();
+                }
+            }
+            else
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return Ok(inactive);
+        }
+
+        [HttpDelete]
+        [Route("DeleteItem")]
+        public async Task<ActionResult<Itens>> DeleteItem(int id)
+        {
+            var deleteItem = _context.Itens.Where(i => i.Id == id).FirstOrDefault();
+
+            if(deleteItem != null)
+            {
+                if(deleteItem.StateCode == true)
+                {
+                    return Ok(new { warning = "Não é possível deletar um item ativo no sistema" });
+                }
+                else 
+                {
+                    _context.Itens.Remove(deleteItem);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            else
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return Ok(id);
+        }
+
+        [HttpPut]
+        [Route("UpdateAmoutItens")]
+        public async Task<ActionResult<Itens>> UpdateAmountItens(int id, int amount)
+        {
+            var item = _context.Itens.Where(i => i.Id == id).FirstOrDefault();
+
+            if(item != null)
+            {
+                item.Amout = item.Amout - amount;
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return Ok(item);
+        }
     }
 }
---
+++ Migrations/20250731002025_ProjectTwo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectTwo.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    StateCode = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Itens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amout = table.Column<int>(type: "int", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    StateCode = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Itens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Itens");
+        }
+    }
+}
